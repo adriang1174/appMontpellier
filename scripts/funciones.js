70d50b7e8901ad586cb853a9ddec7cbe4512d938
adriang1174 <adriang@192.168.1.6>
2014-06-26 23:07:35 -0300

    osvald fixes

--- scripts/funciones.js
+++ scripts/funciones.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
 	$('#seccionTwitter .container').html('<a class="twitter-timeline" href="https://twitter.com/ResidentesArg" data-tweet-limit="7" height="'+altoRedes+'" data-widget-id="467785476154200064" data-chrome="nofooter noborders transparent">Tweets por @ResidentesArg</a>');
 	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
 
-	$( document ).on( "vmousedown", ".botonSeccion", function() {
+	$( document ).on( "vclick", ".botonSeccion", function() {
 		seccion=$(this).data( "seccion" );
 		
 		//Cambiamos el alto dinamicamente
@@ -36,9 +36,11 @@ $(document).ready(function () {
 			
 			if(seccion=="#seccionProximosEventos") {
 				getEvents();
-			}	
-			altoNuevo=$( seccion+" .container").outerHeight()+"px";
-			$( seccion ).css( "height", altoNuevo );
+			}	else {
+				altoNuevo=$( seccion+" .container").outerHeight()+"px";
+				$( seccion ).css( "height", altoNuevo );
+			}
+			
 		}
 	});
 	
@@ -79,24 +81,77 @@ $(document).ready(function () {
 	});	
 	
 	$(".modalbox").fancybox();
+	
+	$( ".eventoMasInfo" ).css({
+		'transform':'translate(500px,0)',
+		'-moz-transform': 'translate(500px,0)',
+		'-webkit-transform': 'translate(500px,0)',
+	});
+	
+	$( document ).on( "vclick", ".botonMasInfo a", function() {
+		eventoNum=$(this).data("evento");
+		$( "#proximosEventos" ).css({
+			'transform':'translate(-500px,0)',
+			'-moz-transform': 'translate(-500px,0)',
+			'-webkit-transform': 'translate(-500px,0)',
+		});
+		$( "#proximosEventos" ).css({
+			'opacity':'0'
+		});
+		$( "#proximosEventos" ).css({
+			'z-index':'0'
+		});
+		$( ".eventoMasInfo:eq("+eventoNum+")" ).css({
+			'display':'block'
+		});
+		$( ".eventoMasInfo:eq("+eventoNum+")" ).css({
+			'transform':'translate(0px,0)',
+			'-moz-transform': 'translate(0px,0)',
+			'-webkit-transform': 'translate(0px,0)',
+		});
+		$( ".eventoMasInfo:eq("+eventoNum+")" ).css({
+			'opacity':'1'
+		});
+		$( '#seccionProximosEventos .container' ).scrollTop( 0 );
+	});
+	
+	$( document ).on( "vclick", ".botonVolver a", function() {
+		$( ".eventoMasInfo" ).css({
+			'transform':'translate(500px,0)',
+			'-moz-transform': 'translate(500px,0)',
+			'-webkit-transform': 'translate(500px,0)',
+		});
+		$( ".eventoMasInfo" ).css({
+			'opacity':'0'
+		});
+		$( "#proximosEventos" ).css({
+			'transform':'translate(0,0)',
+			'-moz-transform': 'translate(0,0)',
+			'-webkit-transform': 'translate(0,0)',
+		});
+		$( "#proximosEventos" ).css({
+			'opacity':'1'
+		});
+		$( "#proximosEventos" ).css({
+			'z-index':'2'
+		});
+	});
 
 });
 
 function getEvents()
 {
-   $.ajax({
-
-     type: "GET",
-     url: 'http:/www.comunidadresidentes.com.ar/appmontpellier/eventos.html',
-     dataType: "html",
-     success: function(data) {
-           // data is ur summary
-		   //alert(data);
-          $('#eventos').html(data);
-		  altoNuevo=$( '#eventos').outerHeight()+"px";
-		  $( '#seccionProximosEventos' ).css( "height", altoNuevo );
-     }
-
-   });
-
+	$.ajax({
+		type: "GET",
+		url: 'http://www.comunidadresidentes.com.ar/appmontpellier/eventos.php',
+		dataType: "html",
+		success: function(data) {
+			// data is ur summary
+			$('#eventos').html(data);
+			altoNuevo=$( window ).height()-150;
+			$( '#seccionProximosEventos' ).css( "height", altoNuevo+"px" );
+			$( '#seccionProximosEventos .container' ).css( "height", altoNuevo-100+"px" );
+			$( '#seccionProximosEventos .container' ).scrollTop( 0 );
+		}
+	});
 }
